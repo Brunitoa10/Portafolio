e08d288d80ae6e94857035c62c637ca6750b4d37
Bruno A Parisi <bnoprs00@gmail.com>
2025-03-22 16:43:12 -0300

    Agregue y modele una imagen en el inicio

--- app/(sections)/Hero.tsx
+++ app/(sections)/Hero.tsx
@@ -1,9 +1,34 @@
+"use client";
+
+import { motion } from "framer-motion";
+import Image from "next/image";
+
 export default function Hero() {
-    return (
-      <section className="text-center py-20">
+  return (
+    <section className="text-center py-20 flex flex-col items-center">
+      <motion.div
+        initial={{ opacity: 0, y: 20 }}
+        animate={{ opacity: 1, y: 0 }}
+        transition={{ duration: 1 }}
+        className="mt-4"
+      >
         <h1 className="text-4xl font-bold">¡Hola! Soy Bruno Ariel Parisi</h1>
-        <p className="text-lg mt-4">Tecnico Informatico y Desarrollador de Software</p>
-      </section>
-    );
-  }
-  
+        <p className="text-lg mt-4">Técnico Informático y Desarrollador de Software</p>
+      </motion.div>
+
+      <motion.div
+        initial={{ opacity: 0, scale: 0.8 }}
+        animate={{ opacity: 1, scale: 1 }}
+        transition={{ duration: 1 }}
+        className="relative w-80 h-80 mt-8"
+      >
+        <Image
+          src="https://ckmtz7p1az.ufs.sh/f/vQEgSxK0T6XMoBmdzZ3hm2KRntxrYiBsJaQULebA9XZG43Ev"
+          alt="Foto de Bruno Ariel Parisi"
+          layout="fill"
+          priority
+        />
+      </motion.div>
+    </section>
+  );
+}
