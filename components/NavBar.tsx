d11568a537003b09ea7188cb101e69952d03a112
Bruno A Parisi <bnoprs00@gmail.com>
2025-03-26 01:54:08 -0300

    Todas las imagenes en uploathing

--- components/NavBar.tsx
+++ components/NavBar.tsx
@@ -7,7 +7,7 @@ import MobileMenu from "./menu/MobileMenu";
 export default function Navbar() {
   return (
     <nav className="p-4 bg-gray-800 flex justify-between items-center">
-      <Image src={"/Recursos/logo3.png"} alt="Mi Portafolio" width={120} height={80} />
+      <Image src={"https://ckmtz7p1az.ufs.sh/f/vQEgSxK0T6XMRTziLOnnMiqBA7EebgzNHDUo5lJWyXKIYCGR"} alt="Mi Portafolio" width={120} height={80} />
       <DesktopMenu />
       <MobileMenu />
     </nav>
