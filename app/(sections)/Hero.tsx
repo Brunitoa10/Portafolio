b072c098c3d965bd076a1f99b5dfb194f2e6af7b
Bruno A Parisi <bnoprs00@gmail.com>
2025-03-26 01:50:26 -0300

    Imagen mia en home provisoria

--- app/(sections)/Hero.tsx
+++ app/(sections)/Hero.tsx
@@ -25,11 +25,11 @@ export default function Hero() {
         initial={{ opacity: 0, scale: 0.8 }}
         animate={{ opacity: 1, scale: 1 }}
         transition={{ duration: 1 }}
-        className="relative w-80 h-80 mt-8"
+        className="relative w-80 h-100 mt-8"
       >
         <Image
           className="max-w-3xl mx-auto p-8 rounded-lg shadow-xl transform transition duration-500 hover:scale-105"
-          src="https://ckmtz7p1az.ufs.sh/f/vQEgSxK0T6XMoBmdzZ3hm2KRntxrYiBsJaQULebA9XZG43Ev"
+          src="https://ckmtz7p1az.ufs.sh/f/vQEgSxK0T6XM4MAjSc8if9ZHzkpua84PeXFOngwGd6msEUQR"
           alt="Foto de Bruno Ariel Parisi"
           fill
           priority
