2b1d9f031ec8a8ff62b3da3a697e51ca3de94bce
Bruno A Parisi <bnoprs00@gmail.com>
2025-03-26 02:53:31 -0300

    update comentarios

--- components/NavBar.tsx
+++ components/NavBar.tsx
@@ -1,4 +1,5 @@
 import Image from "next/image";
+import Link from "next/link";
 import "../styles/buttonStyles.css";
 import "../styles/globals.css";
 import DesktopMenu from "./menu/DesktopMenu";
@@ -7,7 +8,15 @@ import MobileMenu from "./menu/MobileMenu";
 export default function Navbar() {
   return (
     <nav className="p-4 bg-gray-800 flex justify-between items-center">
-      <Image src={"https://ckmtz7p1az.ufs.sh/f/vQEgSxK0T6XMRTziLOnnMiqBA7EebgzNHDUo5lJWyXKIYCGR"} alt="Mi Portafolio" width={120} height={80} />
+      <Link href="/">
+        <Image
+          src="https://ckmtz7p1az.ufs.sh/f/vQEgSxK0T6XMRTziLOnnMiqBA7EebgzNHDUo5lJWyXKIYCGR"
+          alt="Mi Portafolio"
+          width={120}
+          height={80}
+          className="cursor-pointer block"
+        />
+      </Link>
       <DesktopMenu />
       <MobileMenu />
     </nav>
